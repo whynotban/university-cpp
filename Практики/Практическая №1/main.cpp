c46be20e0a8210e040244a879cd55e753de659a5
evans <85763913+whynotban@users.noreply.github.com>
2025-10-11 14:15:23 +0300

    Add files via upload

---
+++ Практики/Практическая №1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+double pi = 3.14159;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double x, y;
+
+    std::cout << "enter x: ";
+    std::cin >> x;
+
+    std::cout << "enter y: ";
+    std::cin >> y;
+
+    double R = 1.0 / tan((pi / 4.0) * x);
+    double S = pow(y, 1.0 / 4.0) * pow(2.0, y);
+
+    double C = std::max(R, S);
+
+    std::cout << "r = " << R << std::endl;
+    std::cout << "s = " << S << std::endl;
+    std::cout << "c = " << C << std::endl;
+
+    system("pause");
+    return 0;
+}
