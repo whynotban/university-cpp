9bd457912cc2c1ff6f9019722e96cce9948cae9e
evans <85763913+whynotban@users.noreply.github.com>
2025-10-23 11:52:48 +0300

    Add files via upload

---
+++ Практики/Практическая №3/main.cpp
@@ -0,0 +1,40 @@
+#include <SFML/Graphics.hpp>
+
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(500, 500), "работа #3");
+
+    int n = 10;  // размерность матрицы
+    int cellSize = 500 / n;
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::White);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
+                cell.setPosition(j * cellSize, i * cellSize);
+                cell.setFillColor(sf::Color::White);
+                cell.setOutlineColor(sf::Color::Black);
+                cell.setOutlineThickness(1.0f);
+
+                // ѕобочна€ диагональ
+                if ((i + j > n - 1) && ((i + j) % 2 != 0)) {
+                    cell.setFillColor(sf::Color::Green);
+                }
+
+                window.draw(cell);
+            }
+        }
+
+        window.display();
+    }
+
+    return 0;
+}
